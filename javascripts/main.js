89b4d5e8e041856c30966664ec15db5485955be3
iz-j <tonomaru39@gmail.com>
2015-05-14 21:38:32 +0900

    First commit for DEMO.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,45 @@
-console.log('This would be the main JS file.');
+// Sample data.
+var DATA = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'NewHampshire',
+  'NewJersey', 'NewMexico', 'NewYork', 'NorthCarolina', 'NorthDakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'RhodeIsland',
+  'SouthCarolina', 'SouthDakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'WestVirginia', 'Wisconsin', 'Wyoming'
+];
+
+// Returns values that start with term.
+var filterData = function(term) {
+  return DATA.filter(function(v) {
+    return v.toLowerCase().lastIndexOf(term.toLowerCase(), 0) === 0;
+  });
+};
+
+var demo1 = new TextAssist(document.getElementById('demo1'), {
+  find: function(term, callback) {
+    callback(filterData(term));
+  }
+});
+
+var demo2 = new TextAssist(document.getElementById('demo2'), {
+  find: function(term, callback) {
+    // Looks like ajax.
+    setTimeout(function() {
+      callback(filterData(term));
+    }, 300);
+  },
+  ulClassName: 'dropdown-menu',
+  anchorClassName: 'dropdown-item',
+  activeClassName: 'active',
+  item: function(value, term) {
+    var capitalized = term;
+    if (term) {
+      capitalized = term.charAt(0).toUpperCase() + term.slice(1);
+    }
+    return value.replace(capitalized, '<u>' + capitalized + '</u>');
+  },
+  loadingHTML: '<img src="images/loading.gif" /><span>Now loading...</span>',
+  noneHTML: '<img src="images/not-found.png" /><span>Nothing found.</span>'
+});
